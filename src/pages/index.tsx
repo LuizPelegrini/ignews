7c5539ea72569226523a0dc369effbbc769cef83
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2023-02-01 22:12:15 +0800

    update: Define Homepage initial structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '@/styles/pages/home.module.scss';
 
 export default function Home() {
   return (
@@ -6,6 +9,23 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90/month</span>
+          </p>
+        </section>
+        <Image
+          src="/images/avatar.svg"
+          width={300}
+          height={500}
+          priority
+          alt="Girl coding"
+        />
+      </main>
     </>
   )
 }
